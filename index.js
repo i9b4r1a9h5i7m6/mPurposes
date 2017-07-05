5323ddc8c77be5a975fae01bdbbc24c1ab006003
i9b4r1a9h5i7m6 <mohamedibrahim35@gmail.com>
2017-07-05 13:35:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.configure(function () {
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/',function(req,res){  
-         res.sendFile(__dirname + "/home.html");  		         
-     });		     
+		     
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
